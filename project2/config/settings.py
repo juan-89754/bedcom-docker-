798d10a1031b995ed3531d2e9e47f28387e8ca62
juan benitez <benitezsanabrijuan@gmail.com>
2026-07-02 10:22:58 -0500

    validacion en el testeo II

--- project2/config/settings.py
+++ project2/config/settings.py
@@ -80,18 +80,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # --------------------------------------------------------------------------
-# 6. BASE DE DATOS (MYSQL - BEDCOM)
+# 6. BASE DE DATOS 
 # --------------------------------------------------------------------------
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bedcom',
-        'USER': 'root',
-        'PASSWORD': '123456789',
-        'HOST': 'db',
-        'PORT': '3306',
+import sys
+if 'test' in sys.argv or 'pytest' in sys.modules or os.getenv('GITHUB_ACTIONS') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'bedcom',
+            'USER': 'root',
+            'PASSWORD': '123456789',
+            'HOST': 'db',
+            'PORT': '3306',
+        }
+    }
+
 
 # --------------------------------------------------------------------------
 # 7. AUTENTICACIÓN Y BLOQUEO
